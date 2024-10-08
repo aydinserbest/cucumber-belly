0626228faaf68e57576abd5b430e610bc56d2b56
aydinserbest <101752975+aydinserbest@users.noreply.github.com>
2024-10-08 18:03:07 +0200

    JUnit4 branch'inde yapılan değişiklikler

--- src/test/java/io/belly/skeleton/RunCucumberTest.java
+++ src/test/java/io/belly/skeleton/RunCucumberTest.java
@@ -1,17 +1,15 @@
 package io.belly.skeleton;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import org.junit.runner.RunWith;
 
-@Suite
-@IncludeEngines("cucumber")
-@SelectPackages("features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.belly.skeleton")
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty"},
+        features = "src/test/resources/features"
+)
 public class RunCucumberTest {
 }
