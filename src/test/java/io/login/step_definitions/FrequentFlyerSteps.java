2fab17ecdb5038723c3eadbda2dfeae3f8db3fd0
aydinserbest <101752975+aydinserbest@users.noreply.github.com>
2024-11-09 00:04:04 +0100

    update

--- src/test/java/io/login/step_definitions/FrequentFlyerSteps.java
+++ src/test/java/io/login/step_definitions/FrequentFlyerSteps.java
@@ -24,17 +24,39 @@ public class FrequentFlyerSteps {
 
 
     @Given("{string} is a {frequentFlyer}")
-    public void defineFrequentFlyerMember(String name, FrequentFlyerMember frequentFlyer) {
-        frequentFlyer.setName(name); // İsmi adımda verilen değerden ayarlıyoruz
-        System.out.println(name + " is a " + frequentFlyer.getLevel() + " Frequent Flyer member");
+    public void defineFrequentFlyerMember(String name, FrequentFlyerMember member) {
+        member.setName(name); // İsmi adımda verilen değerden ayarlıyoruz
+        System.out.println(name + " is a " + member.getLevel() + " Frequent Flyer member");
 
         // Örnek puan kazanımı
-        frequentFlyer.earnPoints(100);
-        System.out.println(name + " has earned points: " + frequentFlyer.getPoints());
+        member.earnPoints(100);
+        System.out.println(name + " has earned points: " + member.getPoints());
     }
     /*
     Cucumber, step definition’daki {string} parametresi ile eşleşebilmesi için
     çift tırnak içine alınmış değerleri arar.
     Çift tırnak kullanılmazsa, undefined hatası verebilir.
+     */
+    /*
+    @ParameterType, feature dosyasındaki "Gold Frequent Flyer member"
+    veya "Silver Frequent Flyer member" gibi ifadeleri tanır.
+(Gold|Silver) kısmı yalnızca Gold veya Silver değerini yakalar
+ve bu değeri level parametresi olarak frequentFlyer metoduna iletir.
+frequentFlyer metodu bu level değerini kullanarak
+bir FrequentFlyerMember nesnesi (domain object) oluşturur ve geri döndürür.
+     */
+    /*
+    Step definition içinde {frequentFlyer} ile FrequentFlyerMember nesnesini (domain object)
+    doğrudan alabiliyoruz.
+Bu sayede feature dosyasındaki adım ("John is a Gold Frequent Flyer member")
+doğrudan FrequentFlyerMember nesnesine dönüştürülmüş oluyor ve adımda parametre olarak kullanabiliyoruz.
+FrequentFlyerMember nesnesine name ayarlanıyor, earnPoints ile puan kazanımı gibi işlemler yapılabiliyor.
+     */
+    /*
+    Bu yapı ile @ParameterType sayesinde feature dosyasındaki ifadeleri
+    FrequentFlyerMember domain nesnesine çevirmiş olduk.
+    Bu yaklaşım, adım tanımlamaları sırasında feature dosyasındaki metinleri
+    doğrudan anlamlı nesnelere dönüştürerek hem okunabilirliği artırır
+    hem de işlemleri domain nesnesi ile doğrudan yapmamıza olanak tanır.
      */
 }
