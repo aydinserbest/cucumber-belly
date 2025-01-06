6d54e3a469103d3f992198e675e846c2920338e9
aydinserbest <101752975+aydinserbest@users.noreply.github.com>
2025-01-06 03:41:34 +0100

    updated

--- src/test/java/io/login/RunCucumberTest.java
+++ src/test/java/io/login/RunCucumberTest.java
@@ -1,22 +1,22 @@
 package io.login;
 
-import org.junit.platform.suite.api.*;
-
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
 //@SelectPackages("features")
 @SelectClasspathResource("features")
 //@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-//@IncludeTags("selectedbooks")  // başa @ YAZMA
-//@ConfigurationParameter(key = "cucumber.filter.tags", value = "@selectedbooks")
+//@IncludeTags("runnerClassTag")  // başa @ YAZMA
+//@ConfigurationParameter(key = "cucumber.filter.tags", value = "@runnerClassTag")
      // This is an alternative to the above line, bu satırla run edince, @selectedbooks tag'li senaryo çalışır.
 //ama çıktıda diğer senaryoların her biri için
 // tek tek -'cucumber.filter.tags=@selectedbooks' did not match this scenario- uyarısı veriyor
 
 //junit-platform.properties dosyası içinden de ayarla bu gibi tag filan run edebiliyoruz
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.login.step_definitions")
+//@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.login.step_definitions")
 
 
 public class RunCucumberTest {
